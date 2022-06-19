2c7a9ba2a5d7e90e46eae0e356529b2e67d2d8fe
Christina <christinaa126@gmail.com>
2022-06-19 11:34:42 -0500

    added mongoose

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require("./config/connection");
-const routes = require("../no-faking-note-taking/routes");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
